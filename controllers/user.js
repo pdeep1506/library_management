5ceb348d4e296dedad16d7320fc89cff2d32bfa7
pdeep1506 <pdeep1506@gmail.com>
2023-04-23 16:50:19 -0400

    added endpoint to get details of all user

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,36 @@
 import userModel from "../models/user.js"
+
+
+
+// all user data
 export const allUsers = async(req,res,next)=>{
     const allUsers = await userModel.find({});
+   
+    for(let i = 0; i < allUsers.length;i++){
+        allUsers[i].password = undefined;        
+    }
+
+    return res.json({error: false, data:{success:true, allUsers}});
+}
+
+//  get  all user data who are admin.
+export const allAdmin = async(req,res,next)=>{
+    const allUsers = await userModel.find({admin:true});
+   
+    for(let i = 0; i < allUsers.length;i++){
+        allUsers[i].password = undefined;        
+    }
+
+    return res.json({error: false, data:{success:true, allUsers}});
+}
+
+//  get all user data who are not admin.
+export const allNotAdmin = async(req,res,next)=>{
+    const allUsers = await userModel.find({admin: false});
+   
+    for(let i = 0; i < allUsers.length;i++){
+        allUsers[i].password = undefined;        
+    }
+
     return res.json({error: false, data:{success:true, allUsers}});
 }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 import express  from 'express';
-import { allUsers } from '../controllers/user.js';
+import { allUsers, allAdmin, allNotAdmin } from '../controllers/user.js';
 import { verifyAdminLogin} from '../middleware/verifyAdmin.js'
-import { verifyLogin } from '../middleware/verifyLogin.js';
+// import { verifyLogin } from '../middleware/verifyLogin.js';
 const route = express.Router();
 
-route.get('/allUsers',verifyLogin,allUsers);
+route.get('/allUsers',verifyAdminLogin,allUsers);
+route.get('/allAdmin',verifyAdminLogin,allAdmin);
+route.get('/allNotAdmin',verifyAdminLogin,allNotAdmin);
 
 //  login
 //  verify account
