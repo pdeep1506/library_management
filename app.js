7126857a9201f23414c3e39f7af2d71445c0bc0f
pdeep1506 <pdeep1506@gmail.com>
2023-04-25 13:38:49 -0400

    working on documentation

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ import express from 'express';
 import 'dotenv/config.js';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-
+import YAML from 'yamljs'
+// import swagerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 const app =express();
 const port = process.env.PORT || 8000;
-
 const MONGODB_URI = `mongodb+srv://${process.env.MONGOOSE_USERNAME}:${process.env.MONGOOSE_PASSWORD}@atlascluster.bve4ouc.mongodb.net/test`;
 // const MONGODB_URI = process.env.MONGOOSE_URL;
+const swaggerSpec = YAML.load('./api.yaml')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(express.json())
 app.use(cookieParser())
 //  route
